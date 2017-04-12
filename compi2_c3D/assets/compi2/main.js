9861403e2c4738906c25b331f13a751f4b70a474
luigitercero <luigitercero3@gmail.com>
2017-04-12 17:10:40 -0600

    esto es compi2

---
+++ compi2_c3D/assets/compi2/main.js
@@ -0,0 +1,14 @@
+
+
+
+import {nodo} from './prueba1';
+function ejecutar(e){
+    
+     var textfield = document.getElementById(e);
+     p.parse(textfield.nodeValue);
+      //let tree = p.parse.treeparser;
+      //alert(tree);
+alert("hola");
+}
+
+
---
+++ compi2_c3D/assets/compi2/prueba1.js
@@ -0,0 +1,9 @@
+export class nodo{
+
+hellow(){
+return "hola";
+
+}
+
+}
+
